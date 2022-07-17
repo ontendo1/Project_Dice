be49de2442c463e57fc6eb8a3154d42b6939b6f6
Xypesz <99351028+Xypesz@users.noreply.github.com>
2022-07-17 13:51:21 +0300

    e

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -9,34 +9,19 @@ public class MainCharacter : MonoBehaviour
     [SerializeField] float speed,jumpSpeed, grav;
     Vector2 mousePos;
     [SerializeField] GameObject checkGroundObj;
-    [SerializeField] GameObject diceMan; 
-    [SerializeField] GameObject headDice,legDice1,legDice2,armDice1,armDice2, trunkDice;
     [SerializeField] bool onGround;
     [SerializeField] GameObject[] vucutParcalari;
     [SerializeField] GameObject[] vucutParcalariParentleri;
+    GameManager _gameManager;
    
     float screenMid;
     private void Awake()
     {
-        diceMan = GameObject.Find("DiceMAN");
-        headDice = GameObject.Find("Head Dice");
-        armDice1 = GameObject.Find("Arm 1 Dice");
-        armDice2 = GameObject.Find("Arm 2 Dice");
-        trunkDice = GameObject.Find("Trunk Dice");
-        legDice1 = GameObject.Find("Leg 1 Dice");
-        legDice2 = GameObject.Find("Leg 2 Dice");
         screenMid = Screen.width / 2;
         rb = GetComponent<Rigidbody>();
     }
     void Start()
     {
-        vucutParcalari[0].transform.localPosition = headDice.transform.localPosition;
-        vucutParcalari[1].transform.localPosition = armDice1.transform.localPosition;
-        vucutParcalari[2].transform.localPosition = armDice2.transform.localPosition;
-        vucutParcalari[3].transform.localPosition = trunkDice.transform.localPosition;
-        vucutParcalari[4].transform.localPosition = legDice1.transform.localPosition;
-        vucutParcalari[5].transform.localPosition = legDice2.transform.localPosition;
-
         vucutParcalari[1].transform.parent = vucutParcalariParentleri[1].transform;
         vucutParcalari[2].transform.parent = vucutParcalariParentleri[1].transform;
     }
